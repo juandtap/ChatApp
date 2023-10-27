890f40bb7dd10e0b2e80a09aa36db80e539df71c
Diego Tapia <jtapiav4@est.ups.edu.ec>
2023-10-26 22:04:06 -0500

    Division de paquetes java; Cliente y Servidor

---
+++ src/main/java/com/ups/client/MainClient.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ups.client;
+
+import com.ups.client.view.ClientViewLogin;
+
+/**
+ *
+ * @author juand
+ */
+public class MainClient {
+    public static void main(String[] args) {
+        var clientView = new ClientViewLogin();
+        clientView.setVisible(true);
+    }
+}
